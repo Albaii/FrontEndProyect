408fc24578c504dfee904675e8632daad3cf103c
Alby Delgado Morales (ext) <alby.delgado.external@eviden.com>
2024-04-17 16:45:17 +0100

    fix: adding function to the code to login and register

--- src/app/loginandregister/loginandregister.component.ts
+++ src/app/loginandregister/loginandregister.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-loginandregister',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './loginandregister.component.html',
   styleUrl: './loginandregister.component.css'
 })
-export class LoginandregisterComponent {
+export class LoginandregisterComponent implements OnInit {
 
-}
+  LoginForm = {
+    username: '',
+    password: ''
+  }
+
+  RegisterForm = {
+    username: '',
+    age: '',
+    email: '',
+    country: '',
+    password: ''
+  }
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  Login(form: string){
+    if(this.LoginForm.username !== '' || this.LoginForm.password ! == ''){
+      console.log('El usuario se ha logeedo')
+    } else {
+      alert('Error al introducir los datos')
+    }
+  }
+
+  Register(form: string) {
+    if (Object.values(this.RegisterForm).every(value => value !== '')) {
+      console.log('El usuario se ha registrado correctamente');
+    } else {
+      alert('Por favor, complete todos los campos');
+    }
+  }
+  }
